22dd35bc0d40235bf7c9735981546975ee52b4d1
To-War <64891989+To-War@users.noreply.github.com>
2021-09-20 19:43:34 +1000

    Update KAPE_collection_full.ps1

--- RTR Scripts/KAPE_collection_full.ps1
+++ RTR Scripts/KAPE_collection_full.ps1
@@ -1,5 +1,5 @@
 $extract = "C:\Windows\Temp\RTR.exe -y -oC:\Windows\Temp\RTR"
 Invoke-Expression "& $extract"
 Start-Sleep -Seconds 5
-$collect = "C:\Windows\Temp\RTR\kape.exe --tsource C: --tdest C:\Windows\Temp\RTR\Collect --vhd collection --target 'McAfee,McAfee_ePO,WindowsDefender,WebBrowsers,EventLogs,EvidenceOfExecution,FileSystem,LnkFilesAndJumpLists,RecycleBinMetadata,RegistryHives,ScheduledTasks,USBDevicesLogs,WBEM,WER'"
+$collect = "C:\Windows\Temp\RTR\kape.exe --tsource C: --tdest C:\Windows\Temp\RTR\Collect --vhd collection --target 'WindowsDefender,WebBrowsers,EventLogs,EvidenceOfExecution,FileSystem,LnkFilesAndJumpLists,RecycleBinMetadata,RegistryHives,ScheduledTasks,USBDevicesLogs,WBEM,WER'"
 Invoke-Expression "& $collect"
